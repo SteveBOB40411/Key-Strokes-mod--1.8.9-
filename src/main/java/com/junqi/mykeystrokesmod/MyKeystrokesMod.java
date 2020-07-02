f163ebf3898737cce08ac4f45f4ffe98c9882f6f
junqiwu02 <junqiwu02@gmail.com>
2020-07-02 07:33:17 -0400

    fixed acceptedminecraftversions

--- src/main/java/com/junqi/mykeystrokesmod/MyKeystrokesMod.java
+++ src/main/java/com/junqi/mykeystrokesmod/MyKeystrokesMod.java
@@ -5,7 +5,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 
-@Mod(modid = MyKeystrokesMod.MODID, version = MyKeystrokesMod.VERSION)
+@Mod(modid = MyKeystrokesMod.MODID, version = MyKeystrokesMod.VERSION, acceptedMinecraftVersions = "[1.8.0,1.8.9]")
 public class MyKeystrokesMod {
     public static final String MODID = "mykeystrokesmod";
     public static final String VERSION = "1.0";
